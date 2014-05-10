3f0a565ba7d974c750a266463f266d0cd32f6d8d
afrezet <afrezet@clever-age.com>
2014-05-10 21:03:19 +0200

    backoffice development

---
+++ src/BlueBear/BackofficeBundle/Controller/LayerController.php
@@ -0,0 +1,59 @@
+<?php
+
+
+namespace BlueBear\BackofficeBundle\Controller;
+
+use BlueBear\BackofficeBundle\Controller\Behavior\ControllerBehavior;
+use BlueBear\CoreBundle\Entity\Map\Layer;
+use BlueBear\CoreBundle\Manager\LayerManager;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class LayerController extends Controller
+{
+    use ControllerBehavior;
+
+    /**
+     * @Template()
+     */
+    public function indexAction()
+    {
+        $layers = $this->getLayerManager()->findAll();
+
+        return [
+            'layers' => $layers
+        ];
+    }
+
+    /**
+     * @Template()
+     */
+    public function editAction(Request $request)
+    {
+        if ($id = $request->get('id')) {
+            $layer = $this->getLayerManager()->find($id);
+            $this->redirect404Unless($layer);
+        } else {
+            $layer = new Layer();
+        }
+        $form = $this->createForm('layer', $layer);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $this->getLayerManager()->save($layer);
+            $this->setMessage('Pencil successfully saved');
+            return '';
+        }
+        return [
+            'form' => $form->createView()
+        ];
+    }
+
+    /**
+     * @return LayerManager
+     */
+    protected function getLayerManager()
+    {
+        return $this->get('bluebear.manager.layer');
+    }
+}
