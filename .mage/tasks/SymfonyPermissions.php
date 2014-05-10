d0cee44429d5ee8b5a4afa8ff948b3369ce4f75f
afrezet <afrezet@clever-age.com>
2014-05-10 23:16:56 +0200

    adding .mage configuration + .gitignore update + adding jquery via composer + grunt

---
+++ .mage/tasks/SymfonyPermissions.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Task;
+
+use Mage\Task\AbstractTask;
+
+class SymfonyPermissions extends AbstractTask
+{
+    /**
+     * Returns the Title of the Task
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return 'Fix symfony2 permissions';
+    }
+
+    /**
+     * Runs the task
+     *
+     * @return boolean
+     * @throws \Exception
+     * @throws \Mage\Task\ErrorWithMessageException
+     * @throws \Mage\Task\SkipException
+     */
+    public function run()
+    {
+        // TODO handle other permissions types
+        $command = "setfacl -R -m u:www-data:rwX -m u:johnkrovitch:rwX app/cache app/logs";
+        $this->runCommandRemote($command);
+        $command2 = "setfacl -dR -m u:www-data:rwx -m u:johnkrovitch:rwx app/cache app/logs";
+        $this->runCommandRemote($command2);
+    }
+}
