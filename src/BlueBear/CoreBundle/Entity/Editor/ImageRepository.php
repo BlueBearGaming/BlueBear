dfdc69a07f90969b91df676534e45e0eddeab9ba
afrezet <afrezet@clever-age.com>
2014-06-02 14:09:21 +0200

    orphan images bug fix

--- src/BlueBear/CoreBundle/Entity/Editor/ImageRepository.php
+++ src/BlueBear/CoreBundle/Entity/Editor/ImageRepository.php
@@ -4,14 +4,28 @@
 namespace BlueBear\CoreBundle\Entity\Editor;
 
 use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\QueryBuilder;
 
 class ImageRepository extends EntityRepository
 {
+    /**
+     * Find orphans images, i.e. images with no linked item
+     *
+     * @param int $pencilId
+     * @return QueryBuilder
+     */
     public function findOrphans($pencilId = 0)
     {
-        $queryBuilder = $this
-            ->createQueryBuilder('image')
-            ->where('image.pencil IS NULL')
+        $subQueryBuilder = new QueryBuilder($this->_em);
+        $queryBuilder = $this->createQueryBuilder('image');
+
+        $queryBuilder
+            ->where($queryBuilder->expr()->notIn('image.id',
+                $subQueryBuilder
+                    ->select('pencil.id')
+                    ->from('BlueBear\CoreBundle\Entity\Map\Pencil', 'pencil')
+                    ->getDQL()
+            ))
             ->orderBy('image.updatedAt', 'DESC');
 
         if ($pencilId) {
