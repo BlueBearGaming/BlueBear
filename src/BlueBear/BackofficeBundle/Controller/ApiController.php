c48845e5e0b4c3d499d450e0b5b141b865c2d223
afrezet <afrezet@clever-age.com>
2014-12-13 01:28:49 +0100

    adding json snippets on api tests

--- src/BlueBear/BackofficeBundle/Controller/ApiController.php
+++ src/BlueBear/BackofficeBundle/Controller/ApiController.php
@@ -3,6 +3,7 @@
 namespace BlueBear\BackofficeBundle\Controller;
 
 use BlueBear\BackofficeBundle\Controller\Behavior\ControllerBehavior;
+use BlueBear\EngineBundle\Event\EngineEvent;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -18,17 +19,29 @@ class ApiController extends Controller
     public function indexAction()
     {
         $form = $this->createForm('engine_event_test');
+        $snippets = $this->getJsonSnippets();
 
         return [
-            'form' => $form->createView()
+            'form' => $form->createView(),
+            'snippets' => $snippets
         ];
     }
 
-    public function testPHPAction(Request $request)
+    protected function getJsonSnippets()
     {
-        $data = [];
+        $events = EngineEvent::getAllowedEvents();
+        $snippets = [];
 
-
-        return new JsonResponse($data);
+        foreach ($events as $event) {
+            $snippet = new \stdClass();
+            if ($event == EngineEvent::ENGINE_ON_MAP_LOAD) {
+                $snippet->mapId = 1;
+                $snippets[$event] = $snippet;
+            } else if ($event == EngineEvent::ENGINE_ON_MAP_SAVE) {
+                $snippet->mapId = 1;
+                $snippets[$event] = $snippet;
+            }
+        }
+        return json_encode($snippets, JSON_PRETTY_PRINT);
     }
 } 
