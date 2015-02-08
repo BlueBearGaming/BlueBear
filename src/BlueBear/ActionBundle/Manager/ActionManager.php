fc0d8d382e620e1d598f76e0e13dc9ba654ef69b
lutangar <johan.dufour@gmail.com>
2015-02-08 21:25:07 +0100

    fix code style, tabs to spaces and add php doc

--- src/BlueBear/ActionBundle/Manager/ActionManager.php
+++ src/BlueBear/ActionBundle/Manager/ActionManager.php
@@ -5,49 +5,78 @@ namespace Sidus\SidusBundle\Action;
 use Sidus\SidusBundle\Entity\Object;
 use Symfony\Component\DependencyInjection\Container;
 
-class ActionManager {
-	
-	/**
-	 * @var Container
-	 */
-	protected $container;
-	
-	protected static $actions;
+/**
+ * Class ActionManager
+ */
+class ActionManager
+{
+    /**
+     * @var Container
+     */
+    protected $container;
 
-	public function __construct(Container $container) {
-		$this->container = $container;
-	}
-	
-	public function getActionsForObject(Object $object){
-		if($object->hasPermission(\Sidus\SidusBundle\Permission\PermissionMask::ADMIN)){
-			return $this->getActions();
-		}
-		$result = [];
-		foreach($this->getActions() as $action){
-			if($object->hasPermission($action->getPermissionMask())){
-				$result[$action->getName()] = $action;
-			}
-		}
-		return $result;
-	}
-	
-	public function getActions(){
-		if(self::$actions){
-			return self::$actions;
-		}
-		$config = $this->container->getParameter('sidus.actions');
-		foreach ($config as $key => $value){
-			$class_name = isset($value['model_class']) ? $value['model_class'] : '\\Sidus\\SidusBundle\\Action\\DefaultAction';
-			self::$actions[$key] = new $class_name($value);
-		}
-		return self::$actions;
-	}
-	
-	public function getAction($name){
-		$actions = $this->getActions();
-		if(isset($actions[$name])){
-			return $actions[$name];
-		}
-		return null;
-	}
+    /**
+     * @var \Traversable
+     */
+    protected static $actions;
+
+    /**
+     * @param Container $container
+     */
+    public function __construct(Container $container)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * @param Object $object
+     *
+     * @return array|\Traversable
+     */
+    public function getActionsForObject(Object $object)
+    {
+        if ($object->hasPermission(\Sidus\SidusBundle\Permission\PermissionMask::ADMIN)) {
+            return $this->getActions();
+        }
+        $result = [];
+        foreach ($this->getActions() as $action) {
+            if ($object->hasPermission($action->getPermissionMask())) {
+                $result[$action->getName()] = $action;
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * @return \Traversable
+     */
+    public function getActions()
+    {
+        if (self::$actions) {
+            return self::$actions;
+        }
+        $config = $this->container->getParameter('sidus.actions');
+        foreach ($config as $key => $value) {
+            $class_name          = isset($value['model_class']) ? $value['model_class'] : '\\Sidus\\SidusBundle\\Action\\DefaultAction';
+            self::$actions[$key] = new $class_name($value);
+        }
+
+        return self::$actions;
+    }
+
+    /**
+     * @param $name
+     *
+     * @return null
+     */
+    public function getAction($name)
+    {
+        $actions = $this->getActions();
+        if (isset($actions[$name])) {
+            return $actions[$name];
+        }
+
+        return null;
+    }
 }
