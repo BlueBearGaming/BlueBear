9748493672992f00e987e0a817f4b14918d2fe0a
afrezet <afrezet@clever-age.com>
2015-02-09 14:03:45 +0100

    fixing configuration for GameBundle

--- src/BlueBear/GameBundle/DependencyInjection/Configuration.php
+++ src/BlueBear/GameBundle/DependencyInjection/Configuration.php
@@ -5,11 +5,6 @@ namespace BlueBear\GameBundle\DependencyInjection;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
-/**
- * This is the class that validates and merges configuration from your app/config files
- *
- * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html#cookbook-bundles-extension-config-class}
- */
 class Configuration implements ConfigurationInterface
 {
     /**
@@ -20,9 +15,27 @@ class Configuration implements ConfigurationInterface
         $treeBuilder = new TreeBuilder();
         $rootNode = $treeBuilder->root('blue_bear_game');
 
-        // Here you should define the parameters that are allowed to
-        // configure your bundle. See the documentation linked above for
-        // more information on that topic.
+        $rootNode
+            ->children()
+                ->arrayNode('entity_type')
+                    ->prototype('array')
+                        ->children()
+                            ->arrayNode('attributes')
+                                ->prototype('scalar')->end()
+                            ->end()
+                        ->end()
+                    ->end()
+                ->end()
+                ->arrayNode('attribute')
+                    ->prototype('array')
+                        ->children()
+                            ->scalarNode('label')->end()
+                            ->scalarNode('type')->end()
+                        ->end()
+                    ->end()
+                ->end()
+            ->end();
+
 
         return $treeBuilder;
     }
