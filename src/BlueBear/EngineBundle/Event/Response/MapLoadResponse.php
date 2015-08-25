321edd106edb1fa4983265936539d744d981a0e9
johnkrovitch <arnaudfrezet@gmail.com>
2015-08-25 20:06:29 +0200

    fixing map load method compatibility

--- src/BlueBear/EngineBundle/Event/Response/MapLoadResponse.php
+++ src/BlueBear/EngineBundle/Event/Response/MapLoadResponse.php
@@ -2,13 +2,8 @@
 
 namespace BlueBear\EngineBundle\Event\Response;
 
-use BlueBear\CoreBundle\Entity\Map\Context;
 use BlueBear\EngineBundle\Event\EventResponse;
 
 class MapLoadResponse extends EventResponse
 {
-    public function setData(Context $context)
-    {
-        $this->data = $context;
-    }
 }
